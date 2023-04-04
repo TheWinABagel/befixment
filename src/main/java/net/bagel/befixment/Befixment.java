8286beb956128ba234c4ddd05ab33dac2ae90e04
MrWinABagel <gogglebobby@gmail.com>
2023-04-04 02:43:47 -0500

    oops

--- src/main/java/net/bagel/befixment/Befixment.java
+++ src/main/java/net/bagel/befixment/Befixment.java
@@ -1,14 +1,10 @@
 package net.bagel.befixment;
 
+import net.bagel.befixment.zenith.AppendZenithTooltips;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.item.Items;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import safro.zenith.ench.EnchModule;
 
 public class Befixment implements ModInitializer {
 	static public final String modid = "befixment";
